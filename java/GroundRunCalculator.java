2dce69acf83ceec1e62a0d815587ee546c9315e3
peytonhobson <peyton.hobson1@gmail.com>
2022-01-26 09:42:38 -0600

    Renamed files to fit function more properly

--- java/GroundRunCalculator.java
+++ java/GroundRunCalculator.java
@@ -5,7 +5,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
-public class BasicCalculator {
+public class GroundRunCalculator {
 
     public static void main(String[] args) throws FileNotFoundException {
 
