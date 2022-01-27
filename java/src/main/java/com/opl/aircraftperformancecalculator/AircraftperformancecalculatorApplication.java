1247e58c5bf478c70c847cc26094f7bf21ef2cd0
peytonhobson <peyton.hobson1@gmail.com>
2022-01-27 17:06:00 -0600

    Added spring intitializr and rearranged files

---
+++ java/src/main/java/com/opl/aircraftperformancecalculator/AircraftperformancecalculatorApplication.java
@@ -0,0 +1,13 @@
+package aircraftperformancecalculator.src.main.java.com.opl.aircraftperformancecalculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AircraftperformancecalculatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AircraftperformancecalculatorApplication.class, args);
+	}
+
+}
---
+++ java/src/test/java/com/opl/aircraftperformancecalculator/AircraftperformancecalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package aircraftperformancecalculator.src.test.java.com.opl.aircraftperformancecalculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AircraftperformancecalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
