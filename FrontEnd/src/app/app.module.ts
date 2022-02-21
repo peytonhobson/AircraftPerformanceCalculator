582ca3be3a7c18332460c760cc7d7a73e545a498
peytonhobson <peyton.hobson1@gmail.com>
2022-02-21 10:23:59 -0600

    Merged new dash

--- FrontEnd/src/app/app.module.ts
+++ FrontEnd/src/app/app.module.ts
@@ -1,34 +1,14 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+
 import { AppRoutingModule } from './app-routing.module';
-import { ReactiveFormsModule } from '@angular/forms';
-import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
-import { ApiService } from './services/api.service';
-import { JwtInterceptor } from './helpers/jwt.interceptor';
-import { ErrorInterceptor } from './helpers/error.interceptor'
 import { AppComponent } from './app.component';
-import { AlertComponent } from './alerts/alert.component';
-import { HomeComponent } from './home/home.component';
 
 @NgModule({
-    imports: [
-        BrowserModule,
-        ReactiveFormsModule,
-        HttpClientModule,
-        AppRoutingModule
-    ],
-    declarations: [
-        AppComponent,
-        AlertComponent,
-        HomeComponent
-    ],
-    providers: [
-        { provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true },
-        { provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true },
-
-        // provider used to create fake backend
-        ApiService
-    ],
-    bootstrap: [AppComponent]
+    declarations: [AppComponent],
+    imports: [BrowserModule, AppRoutingModule, HttpClientModule],
+    providers: [],
+    bootstrap: [AppComponent],
 })
-export class AppModule { };
+export class AppModule {}
---
+++ FrontEnd/src/modules/icons/icons.font-awesome-solid.ts
@@ -0,0 +1,39 @@
+/* All free solid fontawesome icons -> https://fontawesome.com/icons?d=gallery&s=solid&m=free */
+
+import {
+    faAngleDown,
+    faAngleRight,
+    faArrowLeft,
+    faBars,
+    faBookOpen,
+    faChartArea,
+    faChartBar,
+    faChartPie,
+    faChevronDown,
+    faChevronUp,
+    faColumns,
+    faSearch,
+    faTable,
+    faTachometerAlt,
+    faUser,
+    faCalculator
+} from '@fortawesome/free-solid-svg-icons';
+
+export const fontAwesomeSolidIcons = {
+    faAngleDown,
+    faAngleRight,
+    faArrowLeft,
+    faBars,
+    faBookOpen,
+    faChartArea,
+    faChartBar,
+    faChartPie,
+    faChevronDown,
+    faChevronUp,
+    faColumns,
+    faSearch,
+    faTable,
+    faTachometerAlt,
+    faCalculator,
+    faUser,
+};
