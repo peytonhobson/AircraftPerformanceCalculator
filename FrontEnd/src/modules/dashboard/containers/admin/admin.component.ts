5e2198cc3850fb93b3f41cc527dedde5966392d1
peytonhobson <peyton.hobson1@gmail.com>
2022-03-22 19:26:57 -0500

    Started Outputting activity log to PDF

--- FrontEnd/src/modules/dashboard/containers/admin/admin.component.ts
+++ FrontEnd/src/modules/dashboard/containers/admin/admin.component.ts
@@ -1,5 +1,5 @@
 import { CdkDragDrop, moveItemInArray, transferArrayItem } from "@angular/cdk/drag-drop";
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit, ViewEncapsulation } from "@angular/core";
 import { ApiService } from "@app/services/api.service";
 import { Attachment } from "@app/models/attachment";
 import { UserService } from '@modules/auth/services';
@@ -11,56 +11,67 @@ import { Pilot } from "@app/models/pilot";
 import { AlertService } from "@app/services/alert.service";
 import { ActivityLog } from "@modules/dashboard/models/activity-log";
 
+
+
 @Component({
     selector: 'admin-dashboard',
     templateUrl: './admin.component.html',
     styleUrls: ['admin.component.scss'],
+    encapsulation: ViewEncapsulation.None,
 })
 export class AdminComponent implements OnInit {
     
   constructor(private apiService: ApiService,
     private alertService: AlertService) {}
 
+  LOG = [];
+
+  // public openPDF(): void {
+  //   let DATA: any = document.getElementById('pdfData');
+  //   html2canvas(DATA).then((canvas) => {
+  //     let fileWidth = 208;
+  //     let fileHeight = (canvas.height * fileWidth) / canvas.width;
+  //     const FILEURI = canvas.toDataURL('image/png');
+  //     let PDF = new jsPDF('p', 'mm', 'a4');
+  //     let position = 0;
+  //     PDF.addImage(FILEURI, 'PNG', 0, position, fileWidth, fileHeight);
+  //     PDF.save('angular-demo.pdf');
+  //   });
+  // }
+
   ngOnInit() {
 
+    const allButton = document.getElementById("AllButton") as HTMLButtonElement;
+    const usernameButton = document.getElementById("UsernameButton") as HTMLButtonElement;
+    const usernameInput = document.getElementById("UsernameInput") as HTMLInputElement;
+    const submitButton = document.getElementById("SubmitButton") as HTMLButtonElement;
+    usernameInput.disabled = true;
+
+    allButton.addEventListener('click', (e) => {
+      usernameButton.className = usernameButton.className.replace('btn-dark', 'btn-secondary');
+      allButton.className = allButton.className.replace('btn-secondary', 'btn-dark')
+      usernameInput.disabled = true;
+    })
+
+    usernameButton.addEventListener('click', (e) => {
+      allButton.className = usernameButton.className.replace('btn-dark', 'btn-secondary');
+      usernameButton.className = allButton.className.replace('btn-secondary', 'btn-dark')
+      usernameInput.disabled = false;
+    })
+
+    submitButton.addEventListener('click', (e) => {
+      this.apiService.get('activity-log/all').subscribe(res => {
+        res.data.activityLogs.forEach(element => {
+
+          this.LOG.push(element);
+        })
+        console.log(this.LOG)
+        console.log()
+      });
+    });
   }
-
-  // buildHtmlTable() {
-
-  //   var activityLog: ActivityLog[];
-  //   var activityList;
-
-  //   this.apiService.get('activity-log/all').subscribe(res => {
-  //     activityList = res.data.activityLogs;
-  //   })
-  //   var columns = addAllColumnHeaders(activityList);
-  
-  //   for (var i = 0; i < myList.length; i++) {
-  //     var row$ = $('<tr/>');
-  //     for (var colIndex = 0; colIndex < columns.length; colIndex++) {
-  //       var cellValue = myList[i][columns[colIndex]];
-  //       if (cellValue == null) cellValue = "";
-  //       row$.append($('<td/>').html(cellValue));
-  //     }
-  //     $(selector).append(row$);
-  //   }
-  // }
-
-  // addAllColumnHeaders(myList, selector) {
-  //   var columnSet = [];
-  //   var headerTr$ = $('<tr/>');
-  
-  //   for (var i = 0; i < myList.length; i++) {
-  //     var rowHash = myList[i];
-  //     for (var key in rowHash) {
-  //       if ($.inArray(key, columnSet) == -1) {
-  //         columnSet.push(key);
-  //         headerTr$.append($('<th/>').html(key));
-  //       }
-  //     }
-  //   }
-  //   $(selector).append(headerTr$);
-  
-  //   return columnSet;
-  // }
 }
+function html2canvas(DATA: any) {
+  throw new Error("Function not implemented.");
+}
+
